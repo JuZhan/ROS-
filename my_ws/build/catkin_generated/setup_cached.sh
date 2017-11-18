0d2ade20457c63092f61ea1f9d50b72430fcc818
Juzhan <1242127904@qq.com>
2017-11-18 12:48:21 +0800

    安了双系统，push一下

--- my_ws/build/catkin_generated/setup_cached.sh
+++ my_ws/build/catkin_generated/setup_cached.sh
@@ -7,11 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/juzhan/ros/workspace/my_ws/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/juzhan/ros/workspace/my_ws/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/juzhan/ros/workspace/my_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/juzhan/ros/workspace/my_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH="/home/juzhan/ros/workspace/my_ws/devel:/opt/ros/indigo"
+export CPATH="/home/juzhan/ros/workspace/my_ws/devel/include:/opt/ros/indigo/include"
+export LD_LIBRARY_PATH="/home/juzhan/ros/workspace/my_ws/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/juzhan/ros/workspace/my_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/juzhan/ros/workspace/my_ws/build"
-export PYTHONPATH="/home/juzhan/ros/workspace/my_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export PYTHONPATH="/home/juzhan/ros/workspace/my_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/juzhan/ros/workspace/my_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/juzhan/ros/workspace/my_ws/src:$ROS_PACKAGE_PATH"
